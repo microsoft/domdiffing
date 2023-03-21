8e41bfd80606aca33f66f0bc48d9cd1a11287957
Vinod Kumar Sharma <vinodsharma@microsoft.com>
2023-03-21 18:43:05 +0530

    Remvoing commented code

--- src/readImage.ts
+++ src/readImage.ts
@@ -2,29 +2,6 @@ import * as fs from 'fs';
 import {PNG} from 'pngjs';
 import * as path from 'path';
 
-// const pngData: Buffer = fs.readFileSync(path.join(__dirname, '../src/test.png'));
-// // console.log(`pngData: ${pngData}`);
-// const png: PNG.PNGWithMetadata = PNG.sync.read(pngData);
-
-// const width: number = png.width;
-// const height: number = png.height;
-// const pixels: Uint8Array = png.data;
-
-// console.log(`pixels[0]: ${png.data[0]}`);
-// console.log(`pixels.length: ${png.data.length}`);
-// console.log(`width: ${width}, height: ${height}`);
-// fs.writeFileSync("test.txt", "utf-8");
-
-// for (let i = 0; i < pixels.length; i += 4) {
-//     const r = pixels[i];
-//     const g = pixels[i + 1];
-//     const b = pixels[i + 2];
-//     // console.log(`Pixel ${i / 4}: R=${r}, G=${g}, B=${b}`);
-//     fs.appendFileSync("test.txt", `Pixel ${i / 4}: R=${r}, G=${g}, B=${b});
-//   }
-// Process the image data as needed
-
-
 export const findCoordinatesToIgnore = (pngData: Buffer, startingIndex: number = 0, endIndex: number = 0, skipBy: number = 0) => {
     const png: PNG.PNGWithMetadata = PNG.sync.read(pngData);
     const width: number = png.width;
